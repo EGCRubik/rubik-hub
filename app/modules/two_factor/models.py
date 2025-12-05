428dc870ad8ed11e6f00a0387c5bba203a48903d
ManunGar <nunogarciamanuel@gmail.com>
2025-12-05 19:52:51 +0100

    feat: implement two-factor authentication processing in routes

--- app/modules/two_factor/models.py
+++ app/modules/two_factor/models.py
@@ -3,6 +3,9 @@ from app import db
 
 class TwoFactor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), unique=True, nullable=False)
+    key = db.Column(db.String(255), nullable=False)
+    qrcode = db.Column(db.String(255), nullable=False)
 
     def __repr__(self):
         return f'TwoFactor<{self.id}>'
--- app/modules/two_factor/routes.py
+++ app/modules/two_factor/routes.py
@@ -1,7 +1,32 @@
-from flask import render_template
+from flask import redirect, render_template, request, url_for, session
 from app.modules.two_factor import two_factor_bp
+from flask_login import current_user, login_user
+from app.modules.auth.services import AuthenticationService
+
+authentication_service = AuthenticationService()
 
 
-@two_factor_bp.route('/two_factor', methods=['GET'])
+@two_factor_bp.route('/two_factor', methods=['GET', 'POST'])
 def index():
+    # Si ya está autenticado, redirige a la página principal
+    if current_user.is_authenticated:
+        return redirect(url_for('public.index'))
+
+    # Si no viene de un login válido, redirige al login
+    pending_user_id = session.get('pending_2fa_user_id')
+    if not pending_user_id:
+        return redirect(url_for('auth.login'))
+    
+    # Si es un POST, procesa el código 2FA
+    if request.method == 'POST' and request.form:
+        user = authentication_service.get_by_id(pending_user_id)
+        if not user:
+            return redirect(url_for('auth.login'))
+        code = request.form.get("two_factor_code")
+        print(f"Verifying 2FA code: {code} for user ID: {pending_user_id}")
+        login_user(user, remember=True)
+        session.pop('pending_2fa_user_id', None)
+        return redirect(url_for('public.index'))
+
+    # Muestra la página de 2FA con el user_id pendiente
     return render_template('two_factor/index.html')
