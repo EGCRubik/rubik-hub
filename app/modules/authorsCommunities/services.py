989a587c95d25ee3282d45f7514406f6739effaa
juamorrio <jmorenorios5@gmail.com>
2025-12-07 15:26:36 +0100

    fix: remove authorsCommunities module and update routing to public view

--- app/modules/authorsCommunities/services.py
+++
@@ -1,7 +0,0 @@
-from app.modules.authorsCommunities.repositories import AuthorscommunitiesRepository
-from core.services.BaseService import BaseService
-
-
-class AuthorscommunitiesService(BaseService):
-    def __init__(self):
-        super().__init__(AuthorscommunitiesRepository())
